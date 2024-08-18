656d43bbe6380f1c98e545e1a34392911108fa98
Maria Junqueira <mariaa.junqueira@gmail.com>
2024-08-18 17:01:29 +0200

    adding group count and remove nationality count from items

--- src/app/components/user-item/user-item.component.ts
+++ src/app/components/user-item/user-item.component.ts
@@ -18,17 +18,4 @@ export class UserItemComponent {
   toggleExpansion(): void {
     this.isExpanded = !this.isExpanded;
   }
-
-  /**
-   * Get the count of users with same nationality
-   */
-  get nationalitiesCount(): number {
-    if (!this.allUsers().length) {
-      return 0
-    }
-
-    return this.allUsers().reduce((acc, user) => {
-      return user.nat === this.user().nat ? acc + 1 : acc
-    }, 0)
-  }
 }
