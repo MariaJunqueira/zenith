566e5990731758ff8150db6e75bd811b109f12d8
Maria Junqueira <mariaa.junqueira@gmail.com>
2024-08-18 23:10:17 +0200

    removing deprecated implementation

--- src/app/pages/user-hub/services/user-hub.resolver.ts
+++ src/app/pages/user-hub/services/user-hub.resolver.ts
@@ -1,16 +1,15 @@
 import { Injectable } from '@angular/core';
-import { Resolve } from '@angular/router';
 
 import { UsersService } from '../../../services/users.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class UserHubResolver implements Resolve<any> {
+export class UserHubResolver {
 
   constructor(private usersService: UsersService) { }
 
   resolve() {
-    return this.usersService.getUsers(1, 5000);
+    return this.usersService.getUsers(1, 100);
   }
 }
